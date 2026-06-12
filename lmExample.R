7af3627976b1cf66dc963a4fce217ef79227e58e
kmirka-ow <meerkatmirka@gmail.com>
2026-06-12 10:14:43 -0600

    Mauna Loa plot

--- lmExample.R
+++ lmExample.R
@@ -7,3 +7,8 @@ summary(fit)
 
 plot(x, y)
 abline(fit, col = 2)
+
+co2 = read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type = 'l', main = "Mauna Loa CO2",
+     xlab = "Time", ylab = "ppm")
+lines(co2$decdate, co2$trend, col = 2, lwd = 2)
